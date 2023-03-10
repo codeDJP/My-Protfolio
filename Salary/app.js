9cea18b7aa45f2703dfffa08533e2728e404795c
Dinusha Jayaranga Prabash Kumara <71733605+codeDJP@users.noreply.github.com>
2023-03-10 22:58:48 +0530

    Update app.js

--- Salary/app.js
+++ Salary/app.js
@@ -1,10 +1,25 @@
+// Define the function
 function calculateBudget(salary) {
-  let transportation = Math.round(salary * 0.175);
-  let food = Math.round(salary * 0.205);
-  let utilities = Math.round(salary * 0.105);
-  let entertainment = Math.round(salary * 0.135);
-  let personal = Math.round(salary * 0.185);
-  let savings = Math.round(salary * 0.195);
-  
-  return { transportation, food, utilities, entertainment, personal, savings };
+  // Calculate budget allocation for each expense category based on percentages
+  const transportation = Math.round(salary * 0.175);
+  const food = Math.round(salary * 0.205);
+  const utilities = Math.round(salary * 0.105);
+  const entertainment = Math.round(salary * 0.135);
+  const personal = Math.round(salary * 0.185);
+  const savings = Math.round(salary * 0.195);
+
+  // Return an object with expense categories as keys and budget allocations as values
+  return {
+    transportation,
+    food,
+    utilities,
+    entertainment,
+    personal,
+    savings
+  };
 }
+
+// Call the function with a sample salary and log the result to the console
+const salary = 50000;
+const budget = calculateBudget(salary);
+console.log(budget); // { transportation: 8750, food: 10250, utilities: 5250, entertainment: 6750, personal: 9250, savings: 9750 }
